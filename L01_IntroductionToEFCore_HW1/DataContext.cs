7bd6f2fb5c5719e8b85c7061971747e203dbb6f3
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-17 11:35:51 +0300

    L01_IntroductionToEFCore_HW1

---
+++ L01_IntroductionToEFCore_HW1/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L01_IntroductionToEFCore_HW1
+{
+    class DataContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=TestEFCoreDb; Trusted_Connection=true");
+        }
+    }
+}
--- L01_IntroductionToEFCore_HW1/Program.cs
+++ L01_IntroductionToEFCore_HW1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace L01_IntroductionToEFCore_HW1
 {
@@ -6,7 +8,28 @@ namespace L01_IntroductionToEFCore_HW1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (DataContext db = new DataContext())
+            {
+                var newBooks = new List<Book>()
+                {
+                    new Book { Name = "The Lord of the Rings", Author = "J. R. R. Tolkien", Year = 1954 },
+                    new Book { Name = "The Hobbit", Author = "J. R. R. Tolkien", Year = 1937 },
+                    new Book { Name = "Dream of the Red Chamber", Author = "Cao Xueqin", Year = 1791 }
+                };
+
+                if (db.Books.FirstOrDefault() == null)
+                {
+                    db.Books.AddRange(newBooks);
+                    db.SaveChanges();
+                }
+
+                var books = db.Books;
+
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"{book.Id} - {book.Name} - {book.Author} - {book.Year}");
+                }
+            }
         }
     }
 }
