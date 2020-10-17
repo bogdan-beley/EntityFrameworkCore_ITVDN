85419d3fe1053af7ce2cc977789947512489c81e
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-17 11:15:24 +0300

    Add project files.

---
+++ L01_IntroductionToEFCore_HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L01_IntroductionToEFCore_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
